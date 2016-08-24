d6359d83aea3aaa7d6c7f502a972a0ba5a7f02da
Juzziee <Juzzwing@gmail.com>
2016-08-24 19:18:16 +1000

    Looting multiple items

--- Assets/Scripts/Players/Loot.cs
+++ Assets/Scripts/Players/Loot.cs
@@ -21,6 +21,15 @@ public class Loot : MonoBehaviour {
 
 	};
 
+	private List<int> lootDictionaryAmounts = new List<int>(){
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+	};
+
 	ItemClass itemObject = new ItemClass();
 
 	private Ray mouseRay;
@@ -35,13 +44,15 @@ public class Loot : MonoBehaviour {
 
 	// Display Dictionary
 	lootDictionary[0] = itemObject.axeItem.name;
+		lootDictionaryAmounts [0] = 10;
 	lootDictionary [1] = itemObject.logItem.name;
+		lootDictionaryAmounts [1] = 10;
 	
 	}
 	
 	void OnGUI() {
 		if (inventoryWindowToggle) {
-			inventoryWindow = GUI.Window (0, inventoryWindow, InventoryWindowMethod, "Corpse - Press 'L' to close");
+			inventoryWindow = GUI.Window (0, inventoryWindow, InventoryWindowMethod, "Corpse - Press 'esc' to close");
 		}
 	}
 
@@ -50,50 +61,94 @@ public class Loot : MonoBehaviour {
 		// Create Inv item boxes
 		GUILayout.BeginArea(new Rect(0, 50, 400, 400));
 
+		// Row 1
 		GUILayout.BeginHorizontal ();
+
+		// Item 1
 		if (GUILayout.Button (lootDictionary [0], GUILayout.Height (50))) {
-			if(lootDictionary[0] != string.Empty){
+			if(lootDictionary[0] != string.Empty && lootDictionaryAmounts[0] != 0){
 				inventoryGUI.inventoryNameDictionary [0] = lootDictionary [0];
+				lootDictionaryAmounts [0] -= 1;
+				inventoryGUI.dictionaryAmounts [0] += 1;
+			}
+			if (lootDictionaryAmounts [0] == 0) {
 				lootDictionary [0] = string.Empty;
 			}
 		}
+		GUILayout.Box (lootDictionaryAmounts [0].ToString (), GUILayout.Height (50));
 
+		// Item 2
 		if (GUILayout.Button (lootDictionary [1], GUILayout.Height (50))) {
-			if(lootDictionary[1] != string.Empty){
+			if(lootDictionary[1] != string.Empty && lootDictionaryAmounts[1] != 0){
 				inventoryGUI.inventoryNameDictionary [1] = lootDictionary [1];
+				lootDictionaryAmounts [1] -= 1;
+				inventoryGUI.dictionaryAmounts [1] += 1;
+			}
+			if (lootDictionaryAmounts [1] == 0) {
 				lootDictionary [1] = string.Empty;
 			}
 		}
+		GUILayout.Box (lootDictionaryAmounts [1].ToString (), GUILayout.Height (50));
 
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		// Item 3
 		if (GUILayout.Button (lootDictionary [2], GUILayout.Height (50))) {
-			if(lootDictionary[2] != string.Empty){
+			if(lootDictionary[2] != string.Empty && lootDictionaryAmounts[3] != 0){
 				inventoryGUI.inventoryNameDictionary [2] = lootDictionary [2];
+				lootDictionaryAmounts [2] -= 1;
+				inventoryGUI.dictionaryAmounts [2] += 1;
+			}
+			if (lootDictionaryAmounts [2] == 0) {
 				lootDictionary [2] = string.Empty;
 			}
 		}
-		GUILayout.EndHorizontal ();
-		GUILayout.BeginHorizontal ();
+		GUILayout.Box (lootDictionaryAmounts [2].ToString (), GUILayout.Height (50));
+
+		// Item 4
 		if (GUILayout.Button (lootDictionary [3], GUILayout.Height (50))) {
-			if(lootDictionary[3] != string.Empty){
+			if(lootDictionary[3] != string.Empty && lootDictionaryAmounts[3] != 0){
 				inventoryGUI.inventoryNameDictionary [3] = lootDictionary [3];
+				lootDictionaryAmounts [3] -= 1;
+				inventoryGUI.dictionaryAmounts [3] += 1;
+			}
+			if (lootDictionaryAmounts [3] == 0) {
 				lootDictionary [3] = string.Empty;
 			}
 		}
+		GUILayout.Box (lootDictionaryAmounts [3].ToString (), GUILayout.Height (50));
 
+
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+
+		// Item 5
 		if (GUILayout.Button (lootDictionary [4], GUILayout.Height (50))) {
-			if(lootDictionary[4] != string.Empty){
+			if(lootDictionary[4] != string.Empty && lootDictionaryAmounts[4] != 0){
 				inventoryGUI.inventoryNameDictionary [4] = lootDictionary [4];
+				lootDictionaryAmounts [4] -= 1;
+				inventoryGUI.dictionaryAmounts [4] += 1;
+			}
+			if (lootDictionaryAmounts [4] == 0) {
 				lootDictionary [4] = string.Empty;
 			}
 		}
+		GUILayout.Box (lootDictionaryAmounts [4].ToString (), GUILayout.Height (50));
 
+		// Item 6
 		if (GUILayout.Button (lootDictionary [5], GUILayout.Height (50))) {
-			if(lootDictionary[5] != string.Empty){
+			if(lootDictionary[5] != string.Empty && lootDictionaryAmounts[5] != 0){
 				inventoryGUI.inventoryNameDictionary [5] = lootDictionary [5];
+				lootDictionaryAmounts [5] -= 1;
+				inventoryGUI.dictionaryAmounts [5] += 1;
+			}
+			if (lootDictionaryAmounts [5] == 0) {
 				lootDictionary [5] = string.Empty;
 			}
 		}
-
+		GUILayout.Box (lootDictionaryAmounts [5].ToString (), GUILayout.Height (50));
 
 		GUILayout.EndHorizontal();
 
@@ -104,7 +159,7 @@ public class Loot : MonoBehaviour {
 	void Update(){
 		mouseRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 
-		if(Input.GetButtonDown("Fire1")){
+		if(Input.GetButtonDown("Interact")){
 			Physics.Raycast(mouseRay, out rayHit);
 			if(rayHit.collider.transform.tag == "Lootable"){
 				inventoryWindowToggle = true;
@@ -115,7 +170,7 @@ public class Loot : MonoBehaviour {
 		}
 
 		// Close loot window
-		if(Input.GetKeyDown("l")) {
+		if(Input.GetButtonDown("Cancel")) {
 			inventoryWindowToggle = false;
 			cursorMode = CursorLockMode.Locked;
 			Cursor.visible = false;
--- Assets/Scripts/Players/inventoryGUI.cs
+++ Assets/Scripts/Players/inventoryGUI.cs
@@ -20,6 +20,15 @@ public class inventoryGUI : MonoBehaviour {
 
 	};
 
+	static public List<int> dictionaryAmounts = new List<int>() {
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+	};
+
 	ItemClass itemObject = new ItemClass();
 
 
@@ -39,16 +48,32 @@ public class inventoryGUI : MonoBehaviour {
 		// Create Inv item boxes
 		GUILayout.BeginArea(new Rect(0, 50, 400, 400));
 
+		// Row 1
 		GUILayout.BeginHorizontal ();
 		GUILayout.Button (inventoryNameDictionary[0], GUILayout.Height (50));
+		GUILayout.Box (dictionaryAmounts [0].ToString (), GUILayout.Height (50));
+
 		GUILayout.Button (inventoryNameDictionary[1], GUILayout.Height (50));
-		GUILayout.Button (inventoryNameDictionary[2], GUILayout.Height (50));
+		GUILayout.Box (dictionaryAmounts [1].ToString (), GUILayout.Height (50));
 		GUILayout.EndHorizontal();
 
+		// Row 2
 		GUILayout.BeginHorizontal ();
+		GUILayout.Button (inventoryNameDictionary[2], GUILayout.Height (50));
+		GUILayout.Box (dictionaryAmounts [2].ToString (), GUILayout.Height (50));
+
+
 		GUILayout.Button (inventoryNameDictionary[3], GUILayout.Height (50));
+		GUILayout.Box (dictionaryAmounts [3].ToString (), GUILayout.Height (50));
+		GUILayout.EndHorizontal ();
+
+		// Row 3
+		GUILayout.BeginHorizontal ();
 		GUILayout.Button (inventoryNameDictionary[4], GUILayout.Height (50));
+		GUILayout.Box (dictionaryAmounts [4].ToString (), GUILayout.Height (50));
+
 		GUILayout.Button (inventoryNameDictionary[5], GUILayout.Height (50));
+		GUILayout.Box (dictionaryAmounts [5].ToString (), GUILayout.Height (50));
 		GUILayout.EndHorizontal();
 
 		GUILayout.EndArea ();
